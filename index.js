3000aa7cdeac4172f2854d4ab468eb968b1f2641
ethan <minionissus@gmail.com>
2026-05-02 17:16:31 +1200

    accidentally edited this instead of live-chat

--- index.js
+++ index.js
@@ -6,10 +6,19 @@ const app = express()
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.post('/join', (req, res) => {
+    console.log('Nickname:', req.body.nickname)
+    const nickname = req.body.nickname
+    res.render('psr', { nickname })
 })
 
 app.listen(3000, () => {
